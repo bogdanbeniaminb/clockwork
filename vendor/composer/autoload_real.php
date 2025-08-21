fc056058ccc143a3c137314554edb4ea650fcfd9
Bogdan Barbu <45259752+bogdanbeniaminb@users.noreply.github.com>
2025-08-21 13:45:58 +0300

    fix: improve PHP version checks and update class properties for better compatibility

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,30 +25,11 @@ class ComposerAutoloaderInit91002f2dc295f0dab1d4a78e1eb710a3
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit91002f2dc295f0dab1d4a78e1eb710a3', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit91002f2dc295f0dab1d4a78e1eb710a3', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit91002f2dc295f0dab1d4a78e1eb710a3::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit91002f2dc295f0dab1d4a78e1eb710a3::getInitializer($loader));
 
         $loader->register(true);
 
